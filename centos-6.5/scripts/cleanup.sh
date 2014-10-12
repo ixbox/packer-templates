63346b67cb1bcc0b00d5f6d38c3be37508e98049
ixbox <ixbox.jp@gmail.com>
2014-10-12 23:11:10 +0900

    centos6

---
+++ centos-6.5/scripts/cleanup.sh
@@ -0,0 +1,36 @@
+yum -y remove gcc gcc-c++ glibc-headers kernel-devel kernel-headers libgomp libstdc++-devel mpfr perl ppl
+#yum -y update --exclude=kernel*
+yum -y clean all
+rm -rf VBoxGuestAdditions_*.iso
+rm -rf /tmp/rubygems-*
+
+SERVICES=(
+    auditd
+    blk-availability
+    # crond
+    ip6tables
+    iptables
+    iscsi
+    iscsid
+    lvm2-monitor
+    mdmonitor
+    multipathd
+    netconsole
+    # netfs
+    # network
+    postfix
+    rdisc
+    restorecond
+    # rsyslog
+    saslauthd
+    # sshd
+    udev-post
+    # vboxadd
+    # vboxadd-service
+    # vboxadd-x11
+)
+for SERVICE in ${SERVICES[@]}; do
+    chkconfig --level 12345 $SERVICE off
+done
+
+sed -i "s/tty\[1-6\]/tty1/" /etc/sysconfig/init
