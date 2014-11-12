e722e267f4134bfe7d7f5b50bbd1e6e56d248aa8
ixbox <ixbox.jp@gmail.com>
2014-11-12 21:22:01 +0900

    add centos7

---
+++ centos7/scripts/vagrant.sh
@@ -0,0 +1,8 @@
+sed -i "s/^\(Defaults.*requiretty\)/#\1/" /etc/sudoers
+
+date > /etc/vagrant_box_build_time
+
+mkdir -pm 700 /home/vagrant/.ssh
+curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
---
+++ centos7/scripts/virtualbox.sh
@@ -0,0 +1,8 @@
+yum -y install bzip2 gcc make gcc-c++ kernel-devel-`uname -r` perl
+
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
